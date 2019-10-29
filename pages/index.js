6ad9e08d86bbdaa07d27bc87de0c54fac4314303
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-10-29 16:29:41 -0400

    Created SSR

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,45 @@
 import React from 'react'
 import axios from 'axios'
 
-function Home() {
-  React.useEffect(() => {
-    getProducts()
-  }, [])
+// This is where the object gets rendered
+function Home({ products }) {
+  console.log(products);
+  // Note the code below in Home method is suited for Client Side Rendering (CSR)
+  // React.useEffect(() => {
+  //   getProducts();
+  // }, []);
 
-  // This is how you resolve most request from the SERVER !!! HOLY SHIT
-  // You need to do a get api call and it returns a promise
-  // The returned promise you can put in a const
-  // Note because this is happening on the back end, we do not know when request will be finished
-  // Hence we use a async to wait for the request to finish before proceeding
-  async function getProducts() {
-    const url = "http://localhost:3000/api/products";
-    const response = await axios.get(url);
-    console.log(response.data);
-  }
-  
+  // async function getProducts() {
+  //   const url = "http://localhost:3000/api/products";
+  //   const response = await axios.get(url);
+  //   console.log(response.data);
+  // }
+
   return <>home</>;
 }
 
 
+// This is how you resolve most request from the SERVER !!! HOLY SHIT
+// You need to do a get api call and it returns a promise
+// The returned promise you can put in a const
+// Note because this is happening on the back end, we do not know when request will be finished
+// Hence we use a async to wait for the request to finish before proceeding
+
+// Code present here is suited for SSR (Server side rendering)
+// This gets some initial data which will be added to the props pf the component itself
+
+Home.getInitialProps = async () => {
+  
+  // 1. fetch data on server
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+  
+  // 2. return response data as an object
+  // note: this object will be merged with existing props
+  return { products: response.data };
+  
+  
+};
 
 export default Home;
+
