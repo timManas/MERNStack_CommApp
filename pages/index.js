f82214dbc0db841b309342c0d7c1178b6d85318c
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-11-16 12:04:41 -0500

    Add pagination to product page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import axios from 'axios'
-import ProductList from '../components/Index/ProductList'
-import baseUrl from '../utils/baseUrl'
+import React from "react";
+import axios from "axios";
+import ProductList from "../components/Index/ProductList";
+import ProductPagination from "../components/Index/ProductPagination";
+import baseUrl from "../utils/baseUrl";
 
 // This is final place after 'render' in the _app.js
-function Home({ products }) {
-  return <ProductList products={products} />
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
 }
-
 
 // This is how you resolve most request from the SERVER !!! HOLY SHIT
 // You need to do a get api call and it returns a promise
@@ -18,18 +23,16 @@ function Home({ products }) {
 // Code present here is suited for SSR (Server side rendering)
 // This gets some initial data which will be added to the props pf the component itself
 
-Home.getInitialProps = async () => {
-
-  // 1. fetch data on server
+Home.getInitialProps = async ctx => {
+  const page = ctx.query.page ? ctx.query.page : "1";
+  const size = 9;
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
-  
+  const payload = { params: { page, size } };
+  // 1. fetch data on server
+  const response = await axios.get(url, payload);
   // 2. return response data as an object
+  return response.data;
   // note: this object will be merged with existing props
-  return { products: response.data };
-  
-  // Note flow is from indexjs -> _app.js for the initialProps
 };
 
 export default Home;
-
